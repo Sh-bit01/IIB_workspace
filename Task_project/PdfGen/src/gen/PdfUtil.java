32ba249b4c5d97aca21845c4d63a3d7d28522dfd
marlin_spilke <shreyash@desk52.com>
2025-04-28 09:49:57 +0530

    apr28_login

--- Task_project/PdfGen/src/gen/PdfUtil.java
+++ Task_project/PdfGen/src/gen/PdfUtil.java
@@ -7,6 +7,11 @@ import com.itextpdf.layout.element.Paragraph;
 
 import java.io.ByteArrayOutputStream;
 
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
+import java.io.InputStream;
+import java.net.URL;
+
 public class PdfUtil {
 
 public static byte[] generatePdfAsBytes(String message) {
@@ -27,4 +32,31 @@ public static byte[] generatePdfAsBytes(String message) {
         return null;
     }
 }
+
+public static byte[] generatePdfWithTextAndImage(String message, String imageUrl) {
+    try {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+
+        PdfWriter writer = new PdfWriter(byteStream);
+        PdfDocument pdfDoc = new PdfDocument(writer);
+        Document document = new Document(pdfDoc);
+
+
+        document.add(new Paragraph(message));
+
+
+        ImageData imageData = ImageDataFactory.create(imageUrl);
+        Image image = new Image(imageData);
+        document.add(image);
+
+        document.close();
+
+        return byteStream.toByteArray();
+    } catch (Exception e) {
+        e.printStackTrace();
+        return null;
+    }
+}
+
+
 }
